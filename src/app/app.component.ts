fa4d2cb3bb8902ed517583cddca533a0c3c7da07
RafiTahmid <rafi.tahmid019@gmail.com>
2023-03-05 00:53:36 +0600

    presentation topics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'presentation-3';
+
+  // Interpolation
+  // data = 'Enlight Solution'
+  // sum = 5 + 5
+
+  // Property Binding
+  // name = 'Enlight'
+  // disable = true
+
+  // Two Way Binding
+  // title = 'Two Way Binding';
+  // data: any
+
+  // Parent to Child
+  // title = 'Parent to Child'
+  // data = 100
+
+  // Child to Parent
+  title = 'Child to Parent'
+  data = 'Parent Compo'
+  updateData(item: string) {
+    console.log(item)
+    this.data = item;
+  }
+
 }
